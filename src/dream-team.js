3791f59133c97ccb34aeea21e246a5d306eee489
fainz777 <fainz777@gmail.com>
2020-02-16 19:54:28 +0300

    feat: tasks solutions

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,10 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!(members instanceof Array)) {
+    return false;
+  }
+  
+  return members.reduce((teamName, username) => {
+    teamName += typeof username === 'string' ? username.trim()[0] : '';
+    return teamName;
+  }, '').toUpperCase().split('').sort().join('');
 };
